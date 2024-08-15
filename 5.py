6a9426c8b56af8b0b31bc2cc6330e7706d55a2cb
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2024-08-14 21:50:33 -0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,13 @@
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a primeira nota: '))
+
+media = (nota1+nota2)/2
+
+print('A média é: ', (media))
+
+if (media >= 6):
+    print("Aluno aprovado!")
+else:
+    print("Aluno reprovado!")
+
+    
