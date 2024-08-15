98bf9eed45cdde0fa419e454414e762979210a85
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2024-08-14 21:39:33 -0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,4 +1,8 @@
 n1 = int(input('Digite o valor a ser dividido: '))
 n2 = int(input('Digite o valor que vai dividir: '))
 
-print(n1, ' / ', n2,' = ', n1/n2)
+
+try:
+    print(n1, ' / ', n2,' = ', n1/n2)
+except ZeroDivisionError:
+    print('Divisão por zero inválida!')
