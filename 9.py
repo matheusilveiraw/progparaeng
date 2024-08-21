78c844fa768a92a7a483a6cf53d8cd48518137c4
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2024-08-21 20:47:50 -0300

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,6 @@
+nomeCompleto = input('Digite o nome completo: ')
+cidade = input('Digite a sua cidade: ')
+estado = input('Digite o seu estado: ')
+dataDeNascimento = input('Digite a sua data de nascimento: ')
+
+print(nomeCompleto , 'de' , cidade,'/'+estado ,'que nasceu em' , dataDeNascimento)
