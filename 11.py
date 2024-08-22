67025db358719c4f886c889a55bbd3612376ebe0
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2024-08-21 21:17:04 -0300

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,8 @@
+deslocamento = float(input('Insira o valor do deslocamento [em metros]: '))
+tempo = float(input('Insira o valor do tempo [em segundos]: '))
+
+velocidadeMedia = deslocamento/tempo
+
+print('A velocidade média distância', deslocamento,'m em',tempo,'segundos é de',velocidadeMedia) 
+
+ 
