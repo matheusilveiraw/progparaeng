01c980ce1b066e4ec295774592fdbc577b15e6d1
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2024-09-01 15:20:11 -0300

    Add files via upload

---
+++ Aula 4/exer2.py
@@ -0,0 +1,11 @@
+#aula 4 exer 2
+
+#2) Escreva um programa que leia uma frase e converta em uma outra
+#variável a cadeia de caracteres de letras maiúsculas em letras
+#minúsculas.
+
+valorInserido = input('Digite uma frase ou palavra: ')
+
+valorTransformado = valorInserido.lower()
+
+print(valorTransformado)
