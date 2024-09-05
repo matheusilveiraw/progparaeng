573f15b0d16349e3c948d73634aca39a14f172a8
matheusilveiraw <59832941+matheusilveiraw@users.noreply.github.com>
2024-09-04 21:20:56 -0300

    Add files via upload

---
+++ Aula 5/aula 5 exemplo 5.py
@@ -0,0 +1,9 @@
+valor = int(input('Digite o valor: '))
+
+if valor > 0:
+    print('o valor é maior que 0!')
+elif valor < 0:
+    print('o valor é menor que 0!')
+else:
+    print('O valor é igual a 0!')
+    
